f7e2773f137024cbb67d2bcad9c98f35964f98d5
Chris West (Faux) <git@goeswhere.com>
2017-10-31 10:39:52 +0000

    extract method

--- parse/src/main.rs
+++ parse/src/main.rs
@@ -23,29 +23,35 @@ fn run() -> Result<()> {
     loop {
         let input = serialize::read_message(&mut stdin, capnp::message::ReaderOptions::new())?;
         let input = input.get_root::<raw_source::Reader>()?;
-
         let mut message = capnp::message::Builder::new_default();
         {
-            let mut output = message.init_root::<source::Builder>();
-            output.set_package(input.get_package()?);
-            output.set_version(input.get_version()?);
-            {
-                let reader = input.get_files()?;
-                let mut builder = output.init_files(reader.len());
-                for i in 0..reader.len() {
-                    let reader = reader.borrow().get(i);
-                    let mut builder = builder.borrow().get(i);
-                    blank_to_null(reader.get_name()?, |x| builder.set_name(x));
-                    builder.set_size(reader.get_size());
-                    blank_to_null(reader.get_md5()?, |x| builder.set_md5(x));
-                    blank_to_null(reader.get_sha1()?, |x| builder.set_sha1(x));
-                    blank_to_null(reader.get_sha256()?, |x| builder.set_sha256(x));
-                    blank_to_null(reader.get_sha512()?, |x| builder.set_sha512(x));
-                }
-            }
+            let output = message.init_root::<source::Builder>();
+            populate_message(input, output)?;
         }
+
         serialize::write_message(&mut stdout, &message)?;
     }
+}
+
+fn populate_message(input: raw_source::Reader, mut output: source::Builder) -> Result<()> {
+    output.set_package(input.get_package()?);
+    output.set_version(input.get_version()?);
+    {
+        let reader = input.get_files()?;
+        let mut builder = output.init_files(reader.len());
+        for i in 0..reader.len() {
+            let reader = reader.borrow().get(i);
+            let mut builder = builder.borrow().get(i);
+            blank_to_null(reader.get_name()?, |x| builder.set_name(x));
+            builder.set_size(reader.get_size());
+            blank_to_null(reader.get_md5()?, |x| builder.set_md5(x));
+            blank_to_null(reader.get_sha1()?, |x| builder.set_sha1(x));
+            blank_to_null(reader.get_sha256()?, |x| builder.set_sha256(x));
+            blank_to_null(reader.get_sha512()?, |x| builder.set_sha512(x));
+        }
+    }
+
+    Ok(())
 }
 
 fn blank_to_null<F>(value: &str, into: F)
