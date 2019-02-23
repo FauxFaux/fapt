a3beae1c8c9800d1ebde635bf688735959495d38
Chris West (Faux) <git@goeswhere.com>
2019-02-23 16:28:30 +0000

    move types tests outside

--- tests/objectify.rs
+++ tests/objectify.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use failure::Error;
 use fapt_pkg::Package;
 
@@ -6,9 +8,49 @@ fn parse(pkg: &str) -> Result<Package, Error> {
 }
 
 #[test]
-fn google_android_installers() -> Result<(), Error> {
+fn parse_multiple_binaries() -> Result<(), Error> {
+    let pkg = parse(include_str!("packages/alien-arena.pkg"))?;
+
+    assert_eq!("7.66+dfsg-5", pkg.version);
+
+    let src = pkg.as_src().unwrap();
+
+    assert_eq!(
+        vec!["alien-arena", "alien-arena-server"],
+        src.binaries
+            .iter()
+            .map(|b| b.name.to_string())
+            .collect::<Vec<String>>()
+    );
+    assert_eq!(HashMap::new(), pkg.unparsed);
+    Ok(())
+}
+
+#[test]
+fn parse_multi_line_package_list() -> Result<(), Error> {
     let pkg = parse(include_str!("packages/google-android-installers.pkg"))?;
     assert_eq!("google-android-installers", pkg.name);
     assert_eq!(31, pkg.as_src().unwrap().binaries.len());
+    Ok(())
+}
+
+/// jessie (and earlier) lack the Package-List section
+#[test]
+fn no_package_list() -> Result<(), Error> {
+    let pkg = parse(include_str!("packages/aa3d.pkg"))?;
+    assert_eq!("aa3d", pkg.name);
+    let bins = &pkg.as_src().unwrap().binaries;
+    assert_eq!(1, bins.len());
+    assert_eq!("aa3d", bins[0].name);
+    Ok(())
+}
+
+#[test]
+fn parse_provides() -> Result<(), Error> {
+    let p = parse(include_str!("packages/python3-cffi-backend.pkg"))?;
+    assert_eq!("python3-cffi-backend", p.name.as_str());
+    let bin = p.as_bin().unwrap();
+    assert_eq!(3, bin.provides.len());
+    assert_eq!(HashMap::new(), p.unparsed);
     Ok(())
 }
