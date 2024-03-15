64614631f2d0ca684e18bd3441aec06ad0c1febc
Chris West (Faux) <git@goeswhere.com>
2024-03-15 12:06:34 +0000

    fix: cli by using derive

--- src/bin/fapt.rs
+++ src/bin/fapt.rs
@@ -3,78 +3,46 @@ use std::io;
 use std::path::Path;
 use std::path::PathBuf;
 
-use anyhow::anyhow;
-use anyhow::bail;
-use anyhow::ensure;
-use anyhow::Context;
-use anyhow::Error;
-use clap::{command, Arg, Command};
+use anyhow::{anyhow, bail, ensure, Context, Error, Result};
+use clap::{command, Parser, Subcommand, ArgAction};
 use fapt::commands;
 use fapt::sources_list;
 use fapt::system::System;
 
-fn main() -> Result<(), anyhow::Error> {
-    let matches = command!()
-        .subcommand_required(true)
-        .arg(
-            Arg::new("sources-list")
-                .long("sources-list")
-                .value_name("PREFIX")
-                .help("explicitly set the sources.list search path"),
-        )
-        .arg(
-            Arg::new("keyring")
-                .long("keyring")
-                .num_args(..)
-                .number_of_values(1)
-                .value_name("PREFIX")
-                .help("explicitly add a keyring search path"),
-        )
-        .arg(
-            Arg::new("cache-dir")
-                .long("cache-dir")
-                .short('c')
-                .value_name("DIRECTORY")
-                .help("explicitly set the cache directory"),
-        )
-        .arg(
-            Arg::new("sources-line")
-                .long("sources-line")
-                .short('r')
-                .value_name("LINE")
-                .num_args(..)
-                .number_of_values(1)
-                .help(concat!(
-                    "a sources.list entry",
-                    " e.g. debs http://deb.debian.org/debian sid main contrib"
-                )),
-        )
-        .arg(
-            Arg::new("arch")
-                .long("arch")
-                .short('a')
-                .value_name("ARCH")
-                .num_args(..)
-                .number_of_values(1)
-                .help("an explicit arch (e.g. 'amd64'); the first provided will be the 'primary'"),
-        )
-        .arg(
-            Arg::new("system-dpkg")
-                .long("system-dpkg")
-                .value_name("PATH")
-                .default_value("/var/lib/dpkg")
-                .help("dpkg database location"),
-        )
-        .subcommand(
-            Command::new("update"), // .help("just fetch necessary data for specified sources"),
-        )
-        .subcommand(
-            Command::new("source-ninja"), // .help("dump out all source packages as ninja"),
-        )
-        .get_matches();
+#[derive(Parser)]
+struct Cli {
+    /// explicitly set the sources.list search path
+    #[clap(long, value_name = "PREFIX")]
+    sources_list: Option<PathBuf>,
+    /// explicitly add a keyring search path
+    #[clap(long, value_name = "PREFIX", number_of_values = 1)]
+    keyring: Option<Vec<PathBuf>>,
+    /// explicitly set the cache directory
+    #[clap(short = 'c', long, value_name = "DIRECTORY")]
+    cache_dir: Option<PathBuf>,
+    /// a sources.list entry, e.g. 'debs http://deb.debian.org/debian sid main contrib'
+    #[clap(short = 'r', long, value_name = "LINE", num_args = 1)]
+    sources_line: Option<Vec<String>>,
+    /// an explicit arch (e.g. 'amd64'); the first provided will be the 'primary'
+    arch: Option<Vec<String>>,
+    /// dpkg database location
+    #[clap(long, value_name = "PATH", default_value = "/var/lib/dpkg" )]
+    system_dpkg: PathBuf,
+    #[command(subcommand)]
+    subcommand: Sub,
+}
+
+#[derive(Subcommand)]
+enum Sub {
+    Update,
+    SourceNinja,
+}
+
+fn main() -> Result<()> {
+    let matches: Cli = Cli::parse();
 
     let mut sources_entries = Vec::with_capacity(16);
-    if let Some(prefix) = matches.get_one::<String>("sources-list") {
+    if let Some(prefix) = matches.sources_list {
         for prefix in expand_dot_d(prefix)? {
             sources_entries.extend(
                 sources_list::read(io::BufReader::new(fs::File::open(&prefix)?))
@@ -83,8 +51,8 @@ fn main() -> Result<(), anyhow::Error> {
         }
     }
 
-    if let Some(lines) = matches.get_many::<String>("sources-line") {
-        for line in lines {
+    if let Some(lines) = matches.sources_line {
+        for line in lines.iter() {
             let entries = sources_list::read(io::Cursor::new(line))
                 .with_context(|| anyhow!("parsing command line: {:?}", line))?;
 
@@ -98,9 +66,9 @@ fn main() -> Result<(), anyhow::Error> {
         }
     }
 
-    let arches = match matches.get_many("arch") {
-        Some(arches) => arches.copied().collect(),
-        None => vec!["amd64"],
+    let arches = match matches.arch {
+        Some(arches) => arches.iter().cloned().collect(),
+        None => vec!["amd64".to_string()],
     };
 
     if sources_entries.is_empty() {
@@ -112,7 +80,7 @@ fn main() -> Result<(), anyhow::Error> {
 
     let mut system = System::cache_only()?;
     system.add_sources_entries(sources_entries.clone().into_iter());
-    if let Some(keyring_paths) = matches.get_raw("keyring") {
+    if let Some(keyring_paths) = matches.keyring {
         for keyring_path in keyring_paths {
             for path in expand_dot_d(keyring_path)? {
                 system.add_keys_from(
@@ -127,16 +95,15 @@ fn main() -> Result<(), anyhow::Error> {
 
     system.set_arches(&arches);
 
-    system.set_dpkg_database(matches.get_one::<String>("system-dpkg").unwrap());
+    system.set_dpkg_database(&matches.system_dpkg);
 
-    match matches.subcommand() {
-        Some(("source-ninja", _)) => {
+    match matches.subcommand {
+        Sub::SourceNinja => {
             commands::source_ninja(&system)?;
         }
-        Some(("update", _)) => {
+        Sub::Update => {
             system.update()?;
         }
-        _ => unreachable!(),
     }
 
     Ok(())
